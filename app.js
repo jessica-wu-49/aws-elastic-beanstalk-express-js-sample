96c81b0da1354c20f4f71f60fcd65a7e0be73fae
jessica-wu-49 <jessica.wu49512@gmail.com>
2025-10-04 07:00:54 +0000

    modify to run test successfully

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`App running on http://localhost:${port}`);
+    });
+}
+
 module.exports = app;
